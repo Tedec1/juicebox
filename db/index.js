a66eb5571f24a845ed5e10836c56f49203a920ab
Tedec1 <jaspermesenbrink@gmail.com>
2021-02-25 22:05:21 -0800

    finished part 1

--- db/index.js
+++ db/index.js
@@ -1,8 +1,36 @@
-const { Client } = require('pg'); // imports the pg module
+const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client("postgres://localhost:5432/juicebox-dev");
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+  `
+  );
+
+  return rows;
+}
+
+async function createUser({ username, password, name, location}) {
+  try {
+    const result = await client.query(
+      `
+    INSERT INTO users(username, password, name, location) VALUES ($1, $2, $3, $4)
+    ON CONFLICT (username) DO NOTHING 
+      RETURNING *;;
+  `,
+      [username, password, name, location]
+    );
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = {
   client,
-}
+  getAllUsers,
+  createUser,
+};
--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,103 @@
-const { client } =require('./index');
+const { client, getAllUsers, createUser } = require("./index");
+const users = [
+  {
+    username: "albert",
+    password: "bertie99",
+    name:'al',
+    location:'SD'
+  },
+  {
+    username: "sandra",
+    password: "2sandy4me",
+    name:'sandra',
+    location:'Denver'
+  },{
+    username:'glamgal',
+    password:'soglam',
+    name:'Gee',
+    location:'San Fransisco'
+  }
+];
+const tables = [
+  {
+    name: "users",
+    columns: `id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true`,
+  },
+];
 
-const testDB = async () => {
-    try {
-        client.connect();
-        const { rows } = await client.query(`SELECT * FROM users;`);
+const createInitialUsers = async (users) => {
+  try {
+    console.log("STARTED inserting users...");
+    await Promise.all(users.map(createUser));
+  } catch (error) {
+    throw error;
+  } finally {
+    console.log("FINISHED inserting users");
+  }
+};
 
-        console.log(rows);
-    } catch (error) {
-        console.error(error);
-    } finally{
-        client.end();
-    }
+async function dropTable(table) {
+  console.log(`dropping table ${table.name}`);
+  await client.query(`
+        DROP TABLE IF EXISTS ${table.name};
+    `);
+  console.log(`table ${table.name} has been dropped`);
+}
+async function dropTables(tablesToDrop) {
+  console.log("Starting to drop tables...");
+  try {
+    await Promise.all(tablesToDrop.map(dropTable));
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    throw Error(`error while dropping tables: ${error.message}`); // we pass the error up to the function that calls dropTables
+  }
 }
 
-testDB();
+// this function should call a query which creates all tables for our database
+async function createTable(table) {
+  await client.query(`
+        CREATE TABLE ${table.name} (${table.columns});
+    `);
+  console.log(`table ${table.name} has been created`);
+}
+async function createTables(tablesToCreate) {
+  console.log("Starting to create tables...");
+  try {
+    await Promise.all(tablesToCreate.map(createTable));
+    console.log("Finished creating tables!");
+  } catch (error) {
+    throw Error(`error while creating tables: ${error.message}`); // we pass the error up to the function that calls dropTables
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+    await dropTables(tables);
+    await createTables(tables);
+    await createInitialUsers(users);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const testDB = async () => {
+  try {
+    // client.connect();
+    const rows = await getAllUsers();
+
+    console.log(rows);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
