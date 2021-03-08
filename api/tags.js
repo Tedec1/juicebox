7868f5e5d25c15a8415dd2ee16590ea39e40e3a4
Tedec1 <jaspermesenbrink@gmail.com>
2021-03-08 14:03:13 -0800

    100% done

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
+const { getAllTags,getPostsByTagName } = require("../db");
 tagsRouter.use((req, res, next) => {
 	console.log("A request is being made to /users");
 
@@ -14,6 +14,26 @@ tagsRouter.get("/", async (req, res) => {
 		tags,
 	});
 });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+	const {tagName} = req.params;
+	console.log(tagName);
+	if(!tagName){
+		next({message:'no tag name'})
+	}
+	try {
+		let posts = await getPostsByTagName(tagName);
+		posts = posts.filter(post => post.active || (req.user && post.author.id === req.user.id));
+	  // use our method to get posts by tag name from the db
+	  res.send({
+		  posts
+	})
+	  // send out an object to the client { posts: // the posts }
+	} catch ({ name, message }) {
+	  next({name,message})
+		// forward the name and message to the error handler
+	}
+  });
 
 module.exports = {
 	tagsRouter,
